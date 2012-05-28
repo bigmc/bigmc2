da98d669b147c73495e7bbbe290c200c294d6b68
Gian Perrone <gian.perrone@gmail.com>
2012-05-29 00:04:16 +0200

    Added skeletal reactive system class

---
+++ src/main/scala/ReactiveSystem.scala
@@ -0,0 +1,13 @@
+package org.bigraph.bigmc
+
+import org.bigraph.bigmc.matcher._
+
+import scala.collection.immutable.Set
+
+class ReactiveSystem(signature : Set[Control], sorting : Sorting) {
+
+}
+
+
+
+
