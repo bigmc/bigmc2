97d1709906b4bb92918864319b5e168b63b324fa
Gian Perrone <gian.perrone@gmail.com>
2012-05-28 18:54:54 +0200

    Added variable assignment generation to allow quantification over sets of nodes

--- src/main/scala/Sorting.scala
+++ src/main/scala/Sorting.scala
@@ -6,10 +6,56 @@ import org.bigraph.bigmc.parser._
 import scala.collection.immutable.Map
 import scala.collection.immutable.Set
 
-/** The core representation of a static bigraph. */
+class VarAssignment(V : Set[Node], vars : List[String]) extends Iterator[Map[String,Node]] {
+    var l : List[Set[Node]] = vars.map(x => V)
+
+    var done = false
+
+    def hasNext = {
+        !done
+    }
+
+    /** Generation the next assignment set. **/
+    def genNext (l : List[Set[Node]]) : List[Set[Node]] = {
+        if(l.size == 0)
+            return Nil
+
+        val h = l.head
+
+        if(h.size == 1) {
+            V :: genNext(l.tail)
+        } else {
+            (h.tail) :: l.tail
+        }
+    }
+
+    def next = {
+        val r = (for(x <- List.range(0,vars.size)) yield {
+            vars(x) -> l(x).head
+        }).toMap
+        
+        done = l.forall(x => x.size == 1)
+
+        l = genNext(l)
+
+        r
+    }
+}
+
+/** A sorting that can be applied to a bigraph. */
 class Sorting(term : LTerm) {
-    def check(b : Bigraph) : Boolean = {
+    
+    def eval (b : Bigraph, vars : Map[String,Node]) : Boolean = {
         false
+    }
+
+    def check(b : Bigraph) : Boolean = {
+        term match {
+            case LPredicate(vars, body) => {
+                val va = new ValAssignment(b.V, vars)
+            }
+            case _ => throw new IllegalArgumentException("Malformed predicate: '" + term + "'")
+        }
     }
 }
 
